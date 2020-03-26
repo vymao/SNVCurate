90861927bdc81aa6e6bda76d31d807d24f528a8f
Victor <victor.mao@berkeley.edu>
2020-03-26 15:40:30 -0400

    Changed HaplotypeCaller

--- HaplotypeCaller_read.py
+++ HaplotypeCaller_read.py
@@ -32,17 +32,34 @@ def parse_args():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-input_path', help='Path to the text file')
     parser.add_argument('-output_path', default='/n/data1/hms/dbmi/park/victor/other/', help='Path to where the final pipeline output will be written to. Default output will be to /n/data1/hms/dbmi/park/victor/other/.')
-    parser.add_argument('-pipeline', default='Germline', help='The corresponding pipeline for the method calls (ie. Somatic or Germline)')
-    parser.add_argument('-queue', default='park', help='The queue used for running jobs')
-    parser.add_argument('-t1', default='5-00:00:00', help='HaplotypeCaller runtime')
-    parser.add_argument('-t2', default='2600', help='Cromwell task runtime')
+    parser.add_argument('-p', '--queue', default='park', help='slurm job submission option')
+    parser.add_argument('-t', '--runtime', default='2-12:00:00', help='slurm job submission option')
     parser.add_argument('-r1', default=0, help='Lower range bound of indices of BAMs to run')
     parser.add_argument('-r2', default=100000, help='Upper range bound of indices of BAMs to run')
-    parser.add_argument('-m', default='victor_mao@hms.harvard.edu', help='Email for notifications')
-    parser.add_argument('-gatk', default='old', help='GATK version: old or new')
-    parser.add_argument('-input_json', default='/n/data1/hms/dbmi/park/victor/scripts/GATK_Germline_SNPs_Indels/haplotypecaller-gvcf-gatk4.hg37.wgs.inputs.json', help='Reference genome-dependent. Path to gatk4-haplotypecaller file')
+    parser.add_argument('--mem_per_cpu', default='20G', help='slurm job submission option')
+    parser.add_argument('--mail_type', default='FAIL', help='slurm job submission option')
+    parser.add_argument('--mail_user', default='victor_mao@hms.harvard.edu', help='slurm job submission option')
+    parser.add_argument('-gatk', '--gatk_path', default='/home/mk446/BiO/Install/GATK4.1.2.0/gatk', help='path to software execution script')
+    parser.add_argument('-reference', '--reference_path', default='/home/mk446/BiO/Install/GATK-bundle/2.8/b37/human_g1k_v37_decoy.fasta', help='path to reference_path file')
+    parser.add_argument('-scatter', '--scatter_size', default='50')
+    parser.add_argument('-n', '--num_cores', default='1', help='slurm job submission option')
 
     return parser.parse_args()
+
+def main(): 
+    tools_dir = '/n/data1/hms/dbmi/park/victor/scripts/'
+    args = vars(parse_args())
+    output_dir, pipeline = arg_clean(args)
+    #move_files(args)
+
+    os.system('module load gcc/6.2.0 python/3.6.0 java')
+
+        with open(args['input_path'], 'r') as f:
+            for index, line in enumerate(f):
+                if not line.isspace() and index in range(int(args['r1']), int(args['r2'])):
+                    os.system('python3 ' + tools_dir + pipeline + ' -in_file ' + line.strip('\n') + ' -out ' + output_dir + ' -t ' + args['t1'] + ' --mem_per_cpu ' args['mem_per_cpu']
+                        + ' -p ' + args['queue'] + ' --mail_user ' + args['mail_user'] + ' -gatk ' + args['gatk_path'] + ' reference ' + args['reference_path'] + ' -scatter ' + args['scatter_size'] + 
+                        ' -n ' + args['num_cores'])
 
 def arg_clean(args):
     """Cleaning the parsed arguments. Specifically, this function does the following: 
@@ -66,56 +83,10 @@ def arg_clean(args):
     output_dir = os.path.join(args['output_path'], filename + '.HaplotypeCaller')
     os.makedirs(output_dir, exist_ok = True)
 
-    if args['pipeline'].lower() == 'somatic':
-        pipeline = 'GATK_Somatic_SNPs_Indels/Mutect2.py'
-    else:
-        if args['gatk'] == 'new':
-            pipeline = 'GATK_Germline_SNPs_Indels/HaplotypeCaller_4.1.2.0.py'
-        elif args['queue'] == 'priopark' or args['queue'] == 'park':
-            pipeline = 'GATK_Germline_SNPs_Indels/HaplotypeCaller_original.py'
-        else:
-            pipeline = 'GATK_Germline_SNPs_Indels/HaplotypeCaller.py'
+    pipeline = 'GATK_Germline_SNPs_Indels/HaplotypeCaller_4.1.2.0.py'
 
     return output_dir, pipeline
 
-def move_files(args):
-    """Moves files to a semi-temporary directory under /n/scratch2/vym1 on the O2 cluster to group all files in a common directory"""
-    home = '/n/scratch2/vym1'
-    filename = re.findall('/[A-Za-z0-9_]*\.', args['input_path'])[0][1:-1]
-    newhome = os.path.join(home, filename)
-    temp_out = os.makedirs(newhome, exist_ok = True)
-
-    with open(args['input_path'], 'r') as f:
-        for line in f:
-            if not line.isspace():
-                parent_directory = re.split('^(.+)/([^/]+)$', line)[1]
-                filename = re.findall('/[A-Za-z0-9_]*\.', line)[0][1:-1]
-                for file in os.listdir(parent_directory):
-                    if filename in file:
-                        cmd = ['sbatch /n/data1/hms/dbmi/park/victor/other/random/Move_file.sh', os.path.join(parent_directory, file.strip('\n')), newhome]
-                        subprocess.Popen(cmd).wait()
-                        #os.system('sbatch /n/data1/hms/dbmi/park/victor/other/random/Move_file.sh ' + os.path.join(parent_directory, file.strip('\n')) + ' ' + newhome)
-
-def main(): 
-    tools_dir = '/n/data1/hms/dbmi/park/victor/scripts/'
-    args = vars(parse_args())
-    output_dir, pipeline = arg_clean(args)
-    #move_files(args)
-
-    os.system('module load gcc/6.2.0 python/3.6.0 java')
-    #temp_out = '/n/scratch2/vym1'  
-
-    if args['pipeline'].lower() == 'somatic':
-        with open(args['input_path'], 'r') as f:
-            for index, line in enumerate(f):
-                if not line.isspace() and index in range(int(args['r1']), int(args['r2'])):
-                    os.system('python3 ' + tools_dir + pipeline + ' -in_file ' + line.strip('\n') + ' -out ' + output_dir + ' -t ' + args['t1'] + ' -t2 ' + args['t2']) 
-    else: 
-        with open(args['input_path'], 'r') as f:
-            for index, line in enumerate(f):
-                if not line.isspace() and index in range(int(args['r1']), int(args['r2'])):
-                    #print('python3 ' + tools_dir + pipeline + ' -in_file ' + line.strip('\n') + ' -out ' + output_dir + ' -t ' + args['t1'] + ' -t2 ' + args['t2'] + ' -p ' + args['queue'] + ' --mail_user ' + args['m'])
-                    os.system('python3 ' + tools_dir + pipeline + ' -in_file ' + line.strip('\n') + ' -out ' + output_dir + ' -t ' + args['t1'] + ' -t2 ' + args['t2'] + ' -p ' + args['queue'] + ' --mail_user ' + args['m'])
 if __name__ == "__main__":
     main()
 
