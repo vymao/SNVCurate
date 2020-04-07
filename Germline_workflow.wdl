a9ff5bcec9618b62a18bce6be2eec4d0df47ea18
Victor <victor.mao@berkeley.edu>
2020-04-07 13:17:07 -0400

    New HaplotypeCaller workflow additions

---
+++ Germline_workflow.wdl
@@ -0,0 +1,218 @@
+## Requirements/expectations :
+## - One analysis-ready BAM file for a single sample (as identified in RG:SM)
+## - Set of variant calling intervals lists for the scatter, provided in a file
+##
+## Outputs :
+## - One GVCF file and its index
+##
+
+# WORKFLOW DEFINITION 
+workflow HaplotypeCallerGvcf_GATK4 {
+  File input_bam
+  File input_bam_index
+  File ref_dict
+  File ref_fasta
+  File ref_fasta_index
+  File sample_list
+  String output_directory
+  
+  Boolean? make_gvcf
+  Boolean making_gvcf = select_first([make_gvcf,true])
+
+  String gatk_docker
+
+  String gatk_path
+  
+  Array[File] scattered_calling = read_lines(sample_list)
+
+  String sample_basename = basename(input_bam, ".bam")
+  
+  String vcf_basename = sample_basename
+
+  String output_suffix = if making_gvcf then ".g.vcf.gz" else ".vcf.gz"
+  String output_filename = vcf_basename + output_suffix
+
+  call HaplotypeCaller {
+    input:
+      input_bam = input_bam,
+      input_bam_index = input_bam_index,
+      output_filename = output_filename,
+      ref_dict = ref_dict,
+      ref_fasta = ref_fasta,
+      ref_fasta_index = ref_fasta_index,
+      make_gvcf = making_gvcf,
+      docker = gatk_docker,
+      gatk_path = gatk_path
+  }
+}
+
+# Merge per-interval GVCFs
+call MergeGVCFs {
+  input:
+    input_vcfs = HaplotypeCaller.output_vcf,
+    input_vcfs_indexes = HaplotypeCaller.output_vcf_index,
+    output_filename = output_filename,
+    docker = gatk_docker,
+    gatk_path = gatk_path,
+    output_directory = output_directory
+}
+
+call GenotypeGVCFs_single {
+  input: 
+    input_vcfs = MergeGVCFs.output_vcf,
+    input_vcfs_indexes = MergeGVCFs.output_vcf_index,
+    output_filename = vcf_basename + ".vcf",
+    docker = gatk_docker,
+    gatk_path = gatk_path,
+    output_directory = output_directory
+}
+
+# Outputs that will be retained when execution is complete
+output {
+  File output_vcf = GenotypeGVCFs.output_vcf
+  File output_vcf_index = GenotypeGVCFs.output_vcf_index
+}
+
+
+# TASK DEFINITIONS
+
+# HaplotypeCaller per-sample in GVCF mode
+task HaplotypeCaller {
+  File input_bam
+  File input_bam_index
+  File interval_list
+
+  String output_filename
+  File ref_dict
+  File ref_fasta
+  File ref_fasta_index
+  Float? contamination
+  Boolean make_gvcf
+
+  String gatk_path
+  String? java_options
+  String java_opt = select_first([java_options, "-XX:GCTimeLimit=50 -XX:GCHeapFreeLimit=10"])
+
+  # Runtime parameters
+  String docker
+  Int? mem_gb
+  Int? disk_space_gb
+  Boolean use_ssd = false
+  Int? preemptible_attempts
+
+  Int machine_mem_gb = select_first([mem_gb, 7])
+  Int command_mem_gb = machine_mem_gb - 1
+
+  command <<<
+  set -e
+  
+    ${gatk_path} --java-options "-Xmx${command_mem_gb}G ${java_opt}" \
+      --spark-master local[${cores}]
+      HaplotypeCaller \
+      -R ${ref_fasta} \
+      -I ${input_bam} \
+      -O ${output_filename} \
+      -contamination ${default=0 contamination} ${true="-ERC GVCF" false="" make_gvcf}
+  >>>
+
+##  runtime {
+##    runtime_minutes: ${runtime}
+##    cpus: ${cores}
+##    requested_memory_mb_per_core: ${memory}
+##    queue: ${queue}
+##  }
+
+  output {
+    File output_vcf = "${output_filename}"
+    File output_vcf_index = "${output_filename}.tbi"
+  }
+}
+
+# Merge GVCFs generated per-interval for the same sample
+task MergeGVCFs {
+  Array[File] input_vcfs
+  Array[File] input_vcfs_indexes
+  String output_filename
+  String output_directory
+
+  String gatk_path
+
+  # Runtime parameters
+  String docker
+  Int? mem_gb
+  Int? disk_space_gb
+  Boolean use_ssd = false
+  Int? preemptible_attempts
+
+  Int machine_mem_gb = select_first([mem_gb, 3])
+  Int command_mem_gb = machine_mem_gb - 1
+
+
+  command <<<
+  set -e
+
+    ${gatk_path} --java-options "-Xmx${command_mem_gb}G"  \
+      MergeVcfs \
+      --INPUT ${sep=' --INPUT ' input_vcfs} \
+      --OUTPUT ${output_directory}${output_filename}
+  >>>
+
+##  runtime {
+##    runtime_minutes: ${runtime}
+##    cpus: ${cores}
+##    requested_memory_mb_per_core: ${memory}
+##    queue: ${queue}
+##  }
+
+
+  output {
+    File output_vcf = "${output_directory}${output_filename}"
+    File output_vcf_index = "${output_directory}${output_filename}.tbi"
+  }
+}
+
+
+task GenotypeGVCFs_single {
+  Array[File] input_vcfs
+  Array[File] input_vcfs_indexes
+  String output_filename
+  String output_directory
+
+  String gatk_path
+
+  # Runtime parameters
+  String docker
+  Int? mem_gb
+  Int? disk_space_gb
+  Boolean use_ssd = false
+  Int? preemptible_attempts
+
+  Int machine_mem_gb = select_first([mem_gb, 3])
+  Int command_mem_gb = machine_mem_gb - 1
+
+
+
+  command <<<
+  set -e
+
+    ${gatk_path} --java-options "-Xmx${command_mem_gb}G"  \
+      GenotypeGVCFs \
+      -R ${ref_fasta} \
+      -V ${input_bam} \
+      -O ${output_filename} \
+  >>>
+
+
+##  runtime {
+##    runtime_minutes: ${runtime}
+##    cpus: ${cores}
+##    requested_memory_mb_per_core: ${memory}
+##    queue: ${queue}
+##  }
+
+
+  output {
+    File output_vcf = "${output_directory}${output_filename}"
+    File output_vcf_index = "${output_directory}${output_filename}.tbi"
+  }
+}
--- HaplotypeCaller.py
+++ HaplotypeCaller.py
@@ -23,6 +23,8 @@ import glob
 import sys
 import fileinput
 from shutil import copyfile
+import json
+
 
 def parse_args():
     """Uses argparse to enable user to customize script functionality"""
@@ -30,22 +32,49 @@ def parse_args():
     parser.add_argument('-in_dir', '--input_directory', help='path to directory containing input files')
     parser.add_argument('-in_file', '--input_file_path', help='path to input file')
     parser.add_argument('-out', '--output_directory', default='./', help='directory to which the output directory "/.HaplotypeCaller/" will be written to')
-    parser.add_argument('-L', '--scattered_calling_intervals_list', default='/n/data1/hms/dbmi/park/alon/software/gatk-test-data/intervals/b37_wgs_scattered_calling_intervals.txt', help='Reference genome-dependent. Scattered calling interval list')
-    parser.add_argument('-n', '--num_cores', default='4', help='slurm job submission option')
-    parser.add_argument('-t', '--runtime', default='10-23:59:00', help='slurm job submission option')
-    parser.add_argument('-t2', '--cromruntime', default='4500', help='cromwell run time; please specify as number of minutes')
+    parser.add_argument('-n', '--num_cores', default='1', help='slurm job submission option')
+    parser.add_argument('-cn', default=10, help='number of cores for Cromwell jobs')
+    parser.add_argument('-t', '--runtime', default='2-00:00:00', help='slurm job submission option')
+    parser.add_argument('-ct', default=3000, help='cromwell run time; please specify as number of minutes')
     parser.add_argument('-p', '--queue', default='long', help='slurm job submission option')
     parser.add_argument('--mem_per_cpu', default='15G', help='slurm job submission option')
+    parser.add_argument('-cm', default='5G', help='cromwell cpu memory per core')
     parser.add_argument('--mail_type', default='FAIL', help='slurm job submission option')
     parser.add_argument('--mail_user', default='victor_mao@hms.harvard.edu', help='slurm job submission option')
-    #parser.add_argument('-overrides', '--overrides_path', default='/n/data1/hms/dbmi/park/alon/software/gatk4-data-processing-master/overrides.conf.non_park', help='path to overrides.conf file')
-    #parser.add_argument('-cromwell', '--cromwell_path', default='/n/data1/hms/dbmi/park/alon/software/cromwell-36.jar', help='path to cromwell.jar file')
-    parser.add_argument('-overrides', '--overrides_path', default='/n/data1/hms/dbmi/park/victor/scripts/GATK_Germline_SNPs_Indels/overrides.conf.new.non_park', help='path to overrides.conf file')
     parser.add_argument('-cromwell', '--cromwell_path', default='/n/shared_db/singularity/hmsrc-gatk/cromwell-43.jar', help='path to cromwell.jar file')
-
-    parser.add_argument('-gatk_wdl', '--gatk4_haplotypecaller_path', default='/n/data1/hms/dbmi/park/alon/software/gatk4-germline-snps-indels-master/haplotypecaller-gvcf-gatk4_shorttime.wdl', help='path to gatk4-haplotypecaller file')
-    parser.add_argument('-input_json', '--input_json_path', default='/n/data1/hms/dbmi/park/alon/software/gatk4-germline-snps-indels-master/haplotypecaller-gvcf-gatk4.hg37.wgs.inputs.json', help='Reference genome-dependent. Path to gatk4-haplotypecaller file')
+    parser.add_argument('-r', default='/home/mk446/BiO/Install/GATK-bundle/2.8/b37/human_g1k_v37_decoy.fasta')
+    parser.add_argument('-gatk', default='/home/mk446/BiO/Install/GATK4.1.2.0//gatk', help='path to software')
     return parser.parse_args()
+
+def main():
+    args = parse_args()
+    clean_arg_paths(args)
+
+    dirname = os.path.dirname(os.path.abspath(__file__))
+    overrides = os.path.join(dirname, 'Overrides.config')
+    wdl = os.path.join(dirname, 'Germline_workflow.wdl')
+    json = os.path.join(dirname, 'Germline_workflow.json') 
+
+    input_files = return_input_files(args, 'bam') if args.input_directory is not None else [args.input_file_path]
+    input_files = sort_by_size(input_files)
+    
+    for input_file in input_files:
+
+        input_json = generate_cromwell_inputs(args, input_file, json, wdl, overrides)
+        slurm_command = return_slurm_command(args)
+        output_file_name = gen_output_file_name(args, input_file)
+        primary_command = return_primary_command(args, output_file_name, input_file, input_json)
+
+        sh_file_name = gen_sh_file_name(args, output_file_name)
+        write_out(args, slurm_command, primary_command, sh_file_name)
+
+        sample_name = ntpath.basename(sh_file_name).split('.bam')[0] + '.g.vcf.gz'  
+        path_to_vcf = os.path.join(ntpath.dirname(ntpath.dirname(ntpath.dirname(sh_file_name))), sample_name)
+
+        if not os.path.isfile(path_to_vcf):
+            #print(path_to_vcf)
+            #submit_job(sh_file_name)
+
 
 def clean_arg_paths(args):
     """Modifies all user-inputted directory paths such that they end with a '/'"""
@@ -67,7 +96,7 @@ def sort_by_size(input_files):
         input_files[i] = input_files[i][0]
     return input_files
 
-def generate_input_json(args, input_file):
+def generate_cromwell_inputs(args, input_file, json, wdl, overrides):
     dir = args.output_directory + '.HaplotypeCaller/' + '.' + os.path.basename(input_file) + '/'
     os.makedirs(dir, exist_ok=True)
 
@@ -76,8 +105,26 @@ def generate_input_json(args, input_file):
     if os.path.isfile(path) and os.access(path, os.R_OK):
         bai_suffix = '.bam.bai'
     
-    copyfile(args.input_json_path, dir + 'input.json')
+    copyfile(json, dir + 'Input.json')
 
+    dict_path = os.path.dirname(args.r)
+    ref = os.path.basename(args.r).split('.fa')[0]
+    
+    with open('input.json') as f:
+        data = f.read()
+        d = json.loads(data)
+        d["HaplotypeCallerGvcf_GATK4.input_bam"] = [input_file]
+        d["HaplotypeCallerGvcf_GATK4.input_bam_index"] = [re.sub('\.bam', bai_suffix, input_file)]
+        d["HaplotypeCallerGvcf_GATK4.output_directory"] = [os.path.join(args.output_directory,'.HaplotypeCaller')]
+        d["HaplotypeCallerGvcf_GATK4.ref_dict"] = [os.path.join(dict_path, ref + '.dict')]
+        d["HaplotypeCallerGvcf_GATK4.ref_fasta"] = [args.r]
+        d["HaplotypeCallerGvcf_GATK4.ref_fasta_index"] = [args.r + '.fai']
+        d["HaplotypeCallerGvcf_GATK4.gatk_path"] = [args.gatk]
+    
+    with open(path_to_json_file, 'w') as f:
+        f.write(json.dumps(d))
+
+"""
     with open(dir + 'input.json', "r") as f:
         for _ in range(13):
             line = f.readline()
@@ -86,11 +133,38 @@ def generate_input_json(args, input_file):
     with fileinput.FileInput(dir + 'input.json', inplace=True) as file:
         for line in file:
             print(line.replace(
-            "gs://gatk-test-data/wgs_bam/NA12878_24RG_hg38/NA12878_24RG_small.hg38.bam", input_file).replace(
-            "gs://gatk-test-data/wgs_bam/NA12878_24RG_hg38/NA12878_24RG_small.hg38.bai", re.sub('.bam', bai_suffix, input_file)).replace(
+            "path/to/bam", input_file).replace(
+            "path/to/bai", re.sub('\.bam', bai_suffix, input_file)).replace(
             "output_dir/", args.output_directory + '.HaplotypeCaller/').replace(
             string, args.scattered_calling_intervals_list), end='')
+"""
+    copyfile(args.overrides_path, dir + 'Overrides.config')
 
+    with fileinput.FileInput(dir + 'overrides.conf', inplace=True) as file:
+        for line in file:
+            print(line.replace(
+            "medium", args.queue).replace(2000, args.ct).replace("8000", args.cm).replace("Int cpus = 1", "Int cpus = " + args.cn), end='')
+        
+
+        runtime-attributes = """
+        Int runtime_minutes = 2000
+        Int cpus = 1
+        String requested_memory_mb_per_core = "8000"
+        String queue = "park"
+        String account_name = "park_contrib"
+        """
+    if args.queue == 'park' or args.queue == 'priopark': 
+        f = open(dir + 'Overrides.config', "r")
+        contents = f.readlines()
+        f.close()
+
+        contents.insert(95, "            --account=${account_name} \\ ")
+
+        f = open(dir + 'Overrides.config', "w")
+        contents = "".join(contents)
+        f.write(contents)
+        f.close()
+"""
     copyfile(args.overrides_path, dir + 'overrides.conf')
     with fileinput.FileInput(dir + 'overrides.conf', inplace=True) as file:
         for line in file:
@@ -99,15 +173,8 @@ def generate_input_json(args, input_file):
     with fileinput.FileInput(dir + 'overrides.conf', inplace=True) as file:
         for line in file:
             print(re.sub(r"runtime_minutes = [0-9]*", 'runtime_minutes = ' + args.cromruntime, line), end='')
-
-    copyfile(args.gatk4_haplotypecaller_path, dir + 'haplotypecaller-gvcf-gatk4.wdl')
-    with fileinput.FileInput(dir + 'haplotypecaller-gvcf-gatk4.wdl', inplace=True) as file:
-        for line in file:
-            print(line.replace(
-            "priopark", args.queue), end='')
-    with fileinput.FileInput(dir + 'haplotypecaller-gvcf-gatk4.wdl', inplace=True) as file:
-        for line in file: 
-            print(re.sub(r'runtime_minutes: \".*\"', 'runtime_minutes: ' + args.cromruntime, line), end='')
+"""
+    copyfile(wdl, os.path.join(dir, 'workflow.wdl'))
 
     return dir + 'input.json'
 
@@ -148,32 +215,6 @@ def submit_job(sh_file_name):
     os.chdir(os.path.dirname(sh_file_name))
     os.system('chmod +x ' + os.path.basename(sh_file_name))
     os.system('sbatch ./' + os.path.basename(sh_file_name))
-
-def main():
-    args = parse_args()
-    clean_arg_paths(args)
-    
-    os.system('module load gcc/6.2.0 python/3.6.0 java')  
-
-    input_files = return_input_files(args, 'bam') if args.input_directory is not None else [args.input_file_path]
-    input_files = sort_by_size(input_files)
-    
-    for input_file in input_files:
-
-        input_json = generate_input_json(args, input_file)
-        slurm_command = return_slurm_command(args)
-        output_file_name = gen_output_file_name(args, input_file)
-        primary_command = return_primary_command(args, output_file_name, input_file, input_json)
-
-        sh_file_name = gen_sh_file_name(args, output_file_name)
-        write_out(args, slurm_command, primary_command, sh_file_name)
-
-        sample_name = ntpath.basename(sh_file_name).split('.bam')[0] + '.g.vcf.gz'  
-        path_to_vcf = os.path.join(ntpath.dirname(ntpath.dirname(ntpath.dirname(sh_file_name))), sample_name)
-
-        if not os.path.isfile(path_to_vcf):
-            #print(path_to_vcf)
-            submit_job(sh_file_name)
 
         
 if __name__ == "__main__":
